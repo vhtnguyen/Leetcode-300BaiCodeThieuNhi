d0a8b01fe8f2712f0cbc792f551c1576913ee735
Stephen Vo <St3phenCS@gmail.com>
2022-07-20 10:45:06 +0700

    #11

--- Lowest CommonAncestorofaBinarySearchTree.cpp
+++ Lowest CommonAncestorofaBinarySearchTree.cpp
@@ -8,28 +8,17 @@ using namespace std;
      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
   };
 
-  
+
 class Solution {
 public:
-    stack<TreeNode*> s;
     TreeNode* lowestCommonAncestor(TreeNode* r, TreeNode* a, TreeNode* b) {
-        BinarySearch(r, a,true);
-        while(!s.empty()){
-            if(BinarySearch(s.top(), b,false))
-                return s.top();
-            else s.pop();
-        }
+       if(r->val<a->val&&r->val<b->val)
+           return lowestCommonAncestor(r->right, a, b);
+        else if(r->val>a->val&&r->val>b->val)
+            return lowestCommonAncestor(r->left, a, b);
+        else 
+            return r;
     }
-    bool BinarySearch(TreeNode* r, TreeNode* x,bool mem) {
-        if(r==nullptr)
-            return false;
-        if(mem)
-            s.push(r);
-        if (r->val == x->val)
-            return true;
-        if (r->val > x->val)
-            return BinarySearch(r->left, x,mem);
-        else
-            return BinarySearch(r->right, x,mem);
-    }
+    
 };
+
