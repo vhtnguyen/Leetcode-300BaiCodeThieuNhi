24e4a898cf461b7aacd342a32ee8b9d087112404
Stephen Vo <St3phenCS@gmail.com>
2022-07-25 00:38:51 +0700

    #23

---
+++ MiddleoftheLinkedList.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         ListNode*f=head,*s=head;
+//         while(f&&f->next){
+//             s=s->next;
+//             f=f->next->next;
+//         }
+//         return s;
+//     }
+// };
